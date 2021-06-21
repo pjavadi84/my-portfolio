fee17889bca55635e7e711004a48a6e9d9428c5c
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-21 16:23:46 -0700

    created testimonial component and scss background color

---
+++ src/components/testimonials/Testimonials.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./testimonials.scss";
+
+class Testimonials extends Component {
+  render() {
+    return <div className="testimonials"></div>;
+  }
+}
+
+export default Testimonials;
