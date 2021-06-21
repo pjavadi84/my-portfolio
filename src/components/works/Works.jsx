95f7b69a5ae418316bab8f6b10ec2dcca8f36dc1
pjavadi84 <pouya.javadi.resume@gmail.com>
2021-06-21 16:27:19 -0700

    adding portfolio component and SCSS

---
+++ src/components/works/Works.jsx
@@ -0,0 +1,10 @@
+import React, { Component } from "react";
+import "./works.scss";
+
+class Works extends Component {
+  render() {
+    return <div className="works"></div>;
+  }
+}
+
+export default Works;
